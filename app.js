1915d9a3c739b7e632b834f5b84dfdfe10b14402
CL3IX <risfarishk@gmail.com>
2022-10-16 16:01:59 +0800

    push score 10 percepat

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var jet = document.getElementById("jet");
 var board = document.getElementById("board");
+const spacebar = 32;
+const kiri = 65;
+const kanan = 68;
 
 // test dari farish
 
@@ -7,13 +10,11 @@ window.addEventListener("keydown", (e) => {
   var left = parseInt(window.getComputedStyle(jet).getPropertyValue("left"));
   if (e.key == "ArrowLeft" && left > 0) {
     jet.style.left = left - 10 + "px";
-  }
-
-  else if (e.key == "ArrowRight" && left <= 460) {
+  } else if (e.key == "ArrowRight" && left <= 460) {
     jet.style.left = left + 10 + "px";
   }
 
-  if (e.key == "ArrowUp" || e.keyCode == 32) {
+  if (e.key == spacebar || e.keyCode == 32) {
     //TOMBOL MENEMBAK
 
     var bullet = document.createElement("div");
@@ -29,17 +30,41 @@ window.addEventListener("keydown", (e) => {
           var rockbound = rock.getBoundingClientRect();
           var bulletbound = bullet.getBoundingClientRect();
 
-
           if (
             bulletbound.left >= rockbound.left &&
             bulletbound.right <= rockbound.right &&
             bulletbound.top <= rockbound.top &&
             bulletbound.bottom <= rockbound.bottom //kiri kanan
           ) {
-            rock.parentElement.removeChild(rock); 
+            rock.parentElement.removeChild(rock);
 
-            document.getElementById("points").innerHTML =
-              parseInt(document.getElementById("points").innerHTML) + 1;
+            let score = (document.getElementById("points").innerHTML =
+              parseInt(document.getElementById("points").innerHTML) + 1);
+            // ini nah lang
+            if (score > 10) {
+              var moverocks = setInterval(() => {
+                var rocks = document.getElementsByClassName("rocks");
+
+                if (rocks != undefined) {
+                  for (var i = 0; i < rocks.length; i++) {
+                    var rock = rocks[i];
+                    var rocktop = parseInt(
+                      window.getComputedStyle(rock).getPropertyValue("top")
+                    ); // OBSTACLE KEBAWAH
+
+                    if (rocktop >= 475) {
+                      alert("Game Over"); // TEXT GAME OVER
+                      clearInterval(moverocks);
+                      window.location.reload();
+                    }
+
+                    rock.style.top = rocktop + 10 + "px"; //MEMPERLAMBAT/MEMPERCEPAT OBSTCLE
+
+                    // if ()
+                  }
+                }
+              }, 90);
+            }
           }
         }
       }
@@ -47,49 +72,50 @@ window.addEventListener("keydown", (e) => {
         window.getComputedStyle(bullet).getPropertyValue("bottom")
       );
 
-
       if (bulletbottom >= 500) {
         clearInterval(movebullet);
       }
       //membatasi bullet di frame
 
-      bullet.style.left = left + "px"; 
+      bullet.style.left = left + "px";
       bullet.style.bottom = bulletbottom + 3 + "px";
     });
   }
 });
 
-var generaterocks = setInterval(() => {// MULAI GAME
+var generaterocks = setInterval(() => {
+  // MULAI GAME
   var rock = document.createElement("div");
   rock.classList.add("rocks");
 
   var rockleft = parseInt(
     window.getComputedStyle(rock).getPropertyValue("left")
-  );   
+  );
 
   rock.style.left = Math.floor(Math.random() * 450) + "px";
 
   board.appendChild(rock);
-}, 1500 );//MEMPERBANYAK/MENGURANGI OBSTCLE 
+}, 1500); //MEMPERBANYAK/MENGURANGI OBSTCLE
 
 var moverocks = setInterval(() => {
   var rocks = document.getElementsByClassName("rocks");
 
   if (rocks != undefined) {
     for (var i = 0; i < rocks.length; i++) {
-
       var rock = rocks[i];
       var rocktop = parseInt(
         window.getComputedStyle(rock).getPropertyValue("top")
-      );// OBSTACLE KEBAWAH
+      ); // OBSTACLE KEBAWAH
 
       if (rocktop >= 475) {
-        alert("Game Over");// TEXT GAME OVER
+        alert("Game Over"); // TEXT GAME OVER
         clearInterval(moverocks);
         window.location.reload();
       }
 
       rock.style.top = rocktop + 10 + "px"; //MEMPERLAMBAT/MEMPERCEPAT OBSTCLE
+
+      // if ()
     }
   }
 }, 450);
